546055cd56851dc0188973b519e868bfbfc5a50c
Sumonahmed69 <sumonahmed012340@gmail.com>
2021-05-30 16:17:34 +0600

    merge

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import "./App.css";
 import AdminPage from "./pages/AdminPage/AdminPage";
-import StudentDashboard from "./pages/StudentDashboard";
 
 function App() {
   return (
-<<<<<<< HEAD
     <div >
       
       <AdminPage></AdminPage> 
       
      
-=======
-    <div>
-      {/* <AdminPage></AdminPage>  */}
-      <StudentDashboard />
->>>>>>> e7e55fad3d015a6a335d5279d72cd12988e6705c
     </div>
   );
 }
