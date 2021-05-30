71dd3c7c47e5f2b1c4e3b1d5a8927e43db3d3bf8
Sumonahmed69 <sumonahmed012340@gmail.com>
2021-05-30 19:05:14 +0600

    footer update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -34,51 +34,8 @@ const Footer = () => {
     </div>
 
 
-
-
-
-
-
-    // <div className="footer-row">
-    //   <div className="container">
-    //     <div className="row">
-    //       <div className="school-logo">
-    //         <h3>PATHSHALA</h3>
-    //         <h6>BETTER WAY TO LEARN &amp; GROW</h6>
-    //       </div>
-    //       <div className="col-md-4 col-sm-6 footer-item">
-    //         <h5>CONTACT US</h5>
-    //         <p>3768 Seabury Ct, Burlington, NC, 27215</p>
-    //         <p>+1 8910000891</p>
-    //         <p>email@pathshala.com</p>
-    //       </div>
-    //       <div className="col-md-2 col-sm-6 footer-item">
-    //         <h5>QUICK LINKS</h5>
-    //         <div className="quick-link-box">
-    //           <a href="#">Academics</a>
-    //           <a href="#">Admission</a>
-    //           <a href="#">Events</a>
-    //           <a href="#">Campus Life</a>
-    //         </div>
-    //       </div>
-    //       <div className="clearfix visible-sm"></div>
-    //       <div className="col-md-3 col-sm-6 footer-item">
-    //         <h5>SCHOOL TIMINGS</h5>
-    //         <p>MON - FRI 9:00 AM - 3:00 PM</p>
-    //         <p>SAT 9:00 AM - 1:00 PM</p>
-    //       </div>
-    //       <div className="col-md-3 col-sm-6 footer-item">
-    //         <h5>SUBSCRIBE</h5>
-    //         <div className="footer-subscribe">
-    //           <input type="text" placeholder="example@pathshala.com" />
-    //         </div>
-    //         <a href="#" className="subscribe-link">
-    //           SUBMIT
-    //         </a>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
+          
+    
   );
 };
 
--- src/components/Home/Testimony.js
+++ src/components/Home/Testimony.js
@@ -5,7 +5,7 @@ import TestimonyCard from './TestimonyCard';
 
 const Testimony = () => {
     return (
-        <div className='my-4 py-5  bg-info  text-center' >
+        <div className=' py-5  bg-info  text-center' >
             <p className='text-light  fw-bold' > <FontAwesomeIcon icon={faStar} /> WE ARE BEST  <FontAwesomeIcon icon={faStar} /> </p>
             <h1 className='text-light my-2  fw-bold'>LATEST NEWS</h1>
             <p className='text-light'>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
