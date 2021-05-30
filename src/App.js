e7e55fad3d015a6a335d5279d72cd12988e6705c
Oshan12345 <bsagor99@gmail.com>
2021-05-30 13:34:19 +0600

    change componentto display in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import AdminPage from './pages/AdminPage/AdminPage';
+import "./App.css";
+import AdminPage from "./pages/AdminPage/AdminPage";
+import StudentDashboard from "./pages/StudentDashboard";
 
 function App() {
   return (
-    <div >
-      
-      <AdminPage></AdminPage> 
-     
+    <div>
+      {/* <AdminPage></AdminPage>  */}
+      <StudentDashboard />
     </div>
   );
 }
